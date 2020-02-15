f8f2c774a7ef8e95cc1750e6bce9b352b39fe040
Madhu Murthy <msmurthy@msn.com>
2020-02-14 22:46:06 -0800

    Add class names for styling footer widgets.

--- footer.php
+++ footer.php
@@ -1,38 +1,63 @@
 <?php
+/**
+* The template for displaying the footer.
+* use the styles in #site-footer for .footer-credits.
+* In my version, #site-footer will have general footer styles like fonts and background color...
+*/
 
 $has_sidebar_1 = is_active_sidebar( 'sidebar-1' );
 $has_sidebar_2 = is_active_sidebar( 'sidebar-2' );
 ?>
 
-<footer>
+<footer id="site-footer" role="contentinfo" class="header-footer-group">
+  <div class="section-inner">
   <!-- Only output the container if there are elements to display. -->
 
-  <?php if( $has_sidebar_1 || $has_sidebar_2 ) ?>
-  <div class="footer-widgets-wrapper">
-    <?php if( $has_sidebar_1 ) { ?>
+    <?php if( $has_sidebar_1 || $has_sidebar_2 ) { ?>
 
-      <div class="footer-widgets column-one">
-        <?php dynamic_sidebar( 'sidebar-1' ); ?>
-      </div>
+    <div class="footer-widgets-outer-wrapper">
+
+      <div class="footer-widgets-wrapper">
+
+        <?php if( $has_sidebar_1 ) { ?>
+
+          <div class="footer-widgets column-one grid-item">
+            <?php dynamic_sidebar( 'sidebar-1' ); ?>
+          </div>
+
+        <?php } ?>
+
+        <?php if ( $has_sidebar_2 ) { ?>
+
+          <div class="footer-widgets column-two grid-item">
+            <?php dynamic_sidebar( 'sidebar-2' ); ?>
+          </div>
+
+        <?php } ?>
+
+      </div> <!-- .footer-widgets-wrapper -->
+
+    </div> <!-- .footer-widgets-outer-wrapper -->
 
     <?php } ?>
 
-    <?php if ( $has_sidebar_2 ) { ?>
+    <div class="footer-credits">
 
-      <div class="footer-widgets column-two">
-        <?php dynamic_sidebar( 'sidebar-2' ); ?>
-      </div>
+      <p class="footer-copyright">&copy;
+        <?php echo date_i18n(
+          /* translators: Copyright date format, see https://secure.php.net/date */
+          _x( 'Y', 'copyright date format', 'simplyblogging' )
+        );
+        ?>
+        <a href="<?php echo esc_url( home_url( '/' )); ?>"><?php bloginfo( 'name' ); ?></a>
+      </p> <!-- .footer-copyright -->
 
-  </div> <!-- .footer-widgets-wrapper -->
+    </div> <!-- .footer-credits -->
 
-  <?php } ?>
-
-  <div>
-    <p>&copy;Simply Blogging. Created by, <a href="http://madhumurthy.info">Madhu Murthy.</a></p>
-  </div>
-
+  </div> <!-- .section-inner -->
 </footer>
 
 <?php wp_footer(); ?>
+
 </body>
 </html>
