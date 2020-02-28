5c1b3e1b7fe05da338acbaec958e8a4ee45c46de
Madhu Murthy <msmurthy@msn.com>
2020-02-27 21:25:53 -0800

    Update div's and classes for styling

--- single.php
+++ single.php
@@ -20,20 +20,18 @@
           <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
             <header class="entry-header">
-              <div class="entry-header-inner section-inner medium">
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <p><?php echo get_the_category_list( ', ' )?></p>
-              </div> <!-- .entry-head-inner -->
+                <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <p class="categories-list"><?php echo get_the_category_list( ', ' )?></p>
             </header>
 
             <figure class="featured-media">
-              <div class="featured-media-inner section-inner medium">
+              <div>
                 <?php the_post_thumbnail(); ?>
               </div> <!-- .featured-media-inner -->
             </figure>
 
             <div class="post-inner">
-              <div class="entry-content section-inner small">
+              <div class="entry-content">
                 <?php
                   the_content();
                 ?>
