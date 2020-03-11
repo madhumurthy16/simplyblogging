04a5a002e47583bc8e451b54e3f7c1f80cfe6cb1
Madhu Murthy <msmurthy@msn.com>
2020-03-10 22:41:05 -0700

    Add search form

--- searchform.php
+++ searchform.php
@@ -2,13 +2,14 @@
 /** The searchform.php template.
 *
 * Used any time that get_search_form() is called.
+
 */
-//$unique_id = simplyblogging_unique_id( 'search-form-' );
 ?>
 
-<!-- <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' )); ?>">
-  <label for="">
-    <input type="search" class="search-field" value="<?php echo get_serch_query(); ?>" name="s" />
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+  <label for="search-form">
+    <span class="screen-reader-text"><?php _e( 'Search for:', 'simplyblogging' ); ?></span>
+    <input type="search" id="search-form" class="search-field" placeholder="Search &hellip;" value="<?php echo get_search_query(); ?>" name="s" />
   </label>
-  <input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'simplyblogging' ); ?>" />
-</form> -->
+  <input type="submit" class="search-submit" value="Search" />
+</form>
