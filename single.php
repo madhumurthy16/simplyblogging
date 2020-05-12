11de93b22b66cede2e098dccd8967dbc622646a6
Madhu Murthy <msmurthy@msn.com>
2020-05-12 11:08:21 -0700

    Update single.php

--- single.php
+++ single.php
@@ -6,47 +6,45 @@
 ?>
 <div class="container">
   <div class="section-inner">
-    <div class="content-outer-wrapper">
-      <div class="content-wrapper">
-        <main id="main-content" role="main">
+    <div class="content-wrapper">
+      <main id="main-content" role="main">
 
-          <?php
-          if( have_posts() ) {
-            while( have_posts() ) {
-              the_post();
-            }
-          } ?>
+        <?php
+        if( have_posts() ) {
+          while( have_posts() ) {
+            the_post();
+          }
+        } ?>
 
-          <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+        <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-            <header class="entry-header">
-                <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <p class="categories-list"><?php echo get_the_category_list( ', ' )?></p>
-            </header>
+          <header class="entry-header">
+              <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+              <p class="categories-list"><?php echo get_the_category_list( ', ' )?></p>
+          </header>
 
-            <figure class="featured-media">
-              <div>
-                <?php the_post_thumbnail(); ?>
-              </div> <!-- .featured-media-inner -->
-            </figure>
+          <figure class="featured-media">
+            <div>
+              <?php the_post_thumbnail(); ?>
+            </div> <!-- .featured-media-inner -->
+          </figure>
 
-            <div class="post-inner">
-              <div class="entry-content">
-                <?php
-                  the_content();
-                ?>
-              </div> <!-- .entry-content -->
-            </div> <!-- .post-inner -->
+          <div class="post-inner">
+            <div class="entry-content">
+              <?php
+                the_content();
+              ?>
+            </div> <!-- .entry-content -->
+          </div> <!-- .post-inner -->
 
-          </article>
-        </main> <!-- #site-content -->
+        </article>
+      </main> <!-- #site-content -->
 
-        <aside id="sidebar-right">
-          <?php dynamic_sidebar( 'sidebar-right' ); ?>
-        </aside>
+      <aside id="sidebar-right">
+        <?php dynamic_sidebar( 'sidebar-right' ); ?>
+      </aside>
 
-      </div>
-    </div> <!-- .content-outer-wrapper -->
+    </div> <!-- .content-wrapper -->
   </div> <!-- .section-inner -->
 </div> <!-- .container -->
 
