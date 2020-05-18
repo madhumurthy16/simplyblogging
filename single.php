ab8ac6e07ebdc830e6ca10fad7dd9bb8eb3df9c0
Madhu Murthy <msmurthy@msn.com>
2020-05-18 12:40:01 -0700

    Add comments template

--- single.php
+++ single.php
@@ -40,8 +40,25 @@
 
         </article>
 
-      <?php get_template_part( 'template-parts/navigation' ); ?>
-      
+      <?php get_template_part( 'template-parts/navigation' );
+
+      /**
+      * Output comments wrapper if it's a post, or if comments are open,
+      * or if there's a comment number - and check for password.
+      * */
+      if( ( is_single() || is_page() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
+        ?>
+
+        <div class="comments-wrapper">
+
+          <?php comments_template() ?>
+
+        </div> <!-- .comments-wrapper -->
+
+        <?php
+      }
+      ?>
+
       </main> <!-- #site-content -->
 
       <aside id="sidebar-right">
