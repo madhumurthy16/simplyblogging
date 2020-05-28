4297df10addf9b70eadbfe9b13c7ca9f50d7d5de
Madhu Murthy <msmurthy@msn.com>
2020-05-27 17:03:43 -0700

    Update modal-menu.php

--- template-parts/modal-menu.php
+++ template-parts/modal-menu.php
@@ -5,8 +5,8 @@
 */
 ?>
 
-<div class="menu-modal section-inner">
-  <nav class="mobile-navigation">
+<div class="menu-modal">
+  <nav class="mobile-navigation section-inner">
     <ul class="modal-menu reset-list-style">
       <?php wp_nav_menu( array (
         'theme_location' => 'mobile'
