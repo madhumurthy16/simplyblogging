ae7ede012fe13b04416bf44196b29865d5879bd3
Madhu Murthy <msmurthy@msn.com>
2020-06-05 12:25:48 -0700

    Add function to generate unique form ID

--- searchform.php
+++ searchform.php
@@ -4,12 +4,19 @@
 * Used any time that get_search_form() is called.
 
 */
+
+/*
+* Generate a unique ID for each form.
+*/
+
+$unique_id = wp_unique_id('search-form-');
 ?>
 
+
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-  <label for="search-form">
+  <label for="<?php echo esc_attr( $unique_id ); ?>">
     <span class="screen-reader-text"><?php _e( 'Search for:', 'simplyblogging' ); ?></span>
-    <input type="search" id="search-form" class="search-field" placeholder="Search &hellip;" value="<?php echo get_search_query(); ?>" name="s" />
+    <input type="search" id="<?php echo esc_attr( $unique_id ); ?>" class="search-field" placeholder="Search &hellip;" value="<?php echo get_search_query(); ?>" name="s" />
   </label>
   <button type="submit" class="search-submit"><i class="fa fa-search" aria-hidden="true"></i></button>
 </form>
