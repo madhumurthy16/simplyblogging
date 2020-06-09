078d9f5a2ddf90696665e1c31cf024511b77f3bb
Madhu Murthy <msmurthy@msn.com>
2020-06-08 23:16:15 -0700

    Create search-form.php

---
+++ template-parts/search-form.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Displays the search form.
+*
+*/
+
+?>
+
+<div class="search-form-wrapper section-inner">
+
+    <?php get_search_form( array (
+      'label' => __( 'Search for:', 'simplyblogging' ),
+    ));
+    ?>
+
+</div> <!-- search-form-wrapper -->
